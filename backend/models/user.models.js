0dbe28e02f3fd3a5bc68f51e171c46367d5bca63
poojabhatia16 <bhatiapooja010101@gmail.com>
2025-06-26 07:25:45 +0530

    Initialize backend with package.json and server.js files, including essential dependencies and scripts.

---
+++ backend/models/user.models.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["student", "admin"],
+      default: "student",
+    },
+    avatar: {
+      type: String,
+      default: "https://i.ibb.co/2yZ6YkN/default-avatar.png", // fallback avatar
+    },
+  },
+  { timestamps: true }
+);
