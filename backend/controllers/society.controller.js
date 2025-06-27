00f584449c2a68e3bc412b83351f1babfdc53d49
poojabhatia16 <bhatiapooja010101@gmail.com>
2025-06-27 19:51:27 +0530

    feat: implement getAllSocieties controller and update package.json scripts

---
+++ backend/controllers/society.controller.js
@@ -0,0 +1,13 @@
+import Society from "../models/society.models";
+
+const getAllSocieties = async (req, res) => {
+  try {
+    const societies = await Society.find();
+    res.status(200).json(societies);
+  } catch (error) {
+    console.error("Error fetching societies:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export { getAllSocieties };
