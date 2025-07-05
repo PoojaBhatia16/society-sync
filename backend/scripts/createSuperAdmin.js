a59fd4004288e13b3271efefc08ed142bea602d9
poojabhatia16 <bhatiapooja010101@gmail.com>
2025-07-05 19:16:30 +0530

    feat: Add createSuperAdmin script for super admin user creation

---
+++ backend/scripts/createSuperAdmin.js
@@ -0,0 +1,36 @@
+// scripts/createSuperAdmin.js
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { User } from "../models/user.models.js";
+import { DB_NAME } from "../constants.js";
+
+dotenv.config({path: ".env"});
+
+const createSuperAdmin = async () => {
+  try {
+    await mongoose.connect(
+          `${process.env.MONGODB_URI}/${DB_NAME}`);
+
+    const existing = await User.findOne({ email: "superadmin@cms.com" });
+    if (existing) {
+      console.log("Superadmin already exists");
+      process.exit(0);
+    }
+
+    const user = await User.create({
+      name: "Super Admin",
+      email: "superadmin@cms.com",
+      password: "kadu123",
+      role: "superadmin",
+      isVerified: true,
+    });
+
+    console.log("Superadmin created:", user.email);
+    process.exit(0);
+  } catch (err) {
+    console.error(" Error creating superadmin:", err);
+    process.exit(1);
+  }
+};
+
+createSuperAdmin();
